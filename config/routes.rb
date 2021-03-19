b17a195d61af2756980f7e1ff36bfe65890b29f7
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2021-03-19 10:40:49 +0900

    developed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+=======
+  get 'users/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+>>>>>>> initial commit
 end
